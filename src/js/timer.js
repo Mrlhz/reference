b3aca9f86acd251cb515e8bb5c1aeed388ff5184
lhz <mr__lhz@163.com>
2018-09-14 00:40:17 +0800

    formatTime

--- src/js/timer.js
+++ src/js/timer.js
@@ -56,3 +56,23 @@ class Time {
 }
 
 export default Time;
+
+const formatTime = date => {
+    const year = date.getFullYear()
+    const month = date.getMonth() + 1
+    const day = date.getDate()
+    const hour = date.getHours()
+    const minute = date.getMinutes()
+    const second = date.getSeconds()
+
+    return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(
+        ':')
+}
+
+const formatNumber = n => {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+
+var d = new Date()
+console.log(formatTime(d))
