b42838f8f63930afc16f05b966cf81433bfda326
mrlhz <mr__lhz@163.com>
2019-08-05 22:38:57 +0800

    壹题 #25 浏览器和 Node 事件循环的区别

---
+++ Daily-Interview-Question/25.js
@@ -0,0 +1,12 @@
+
+/**
+ * @description
+ * @see https://nodejs.org/zh-cn/docs/guides/event-loop-timers-and-nexttick/ 浏览器与Node的事件循环(Event Loop)有何区别?
+ * @see https://juejin.im/post/5c337ae06fb9a049bc4cd218#heading-12 Node.js 事件循环，定时器和 process.nextTick()
+ * @see https://html.spec.whatwg.org/multipage/webappapis.html#event-loops html#event-loops
+ *
+ */
+
+
+// test
+// 看03.js
