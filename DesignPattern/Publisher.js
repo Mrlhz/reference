c7db3d1b6be041000e890d180a236fee00025fa4
mrlhz <mr__lhz@163.com>
2022-04-19 01:21:50 +0800

    发布－订阅模式的通用实现

--- DesignPattern/Publisher.js
+++ DesignPattern/Publisher.js
@@ -10,22 +10,27 @@
 class Publisher {
   constructor() {
     this.subsMap = new Map() // 存储订阅者
+    this.offlineStack = []
     this.id = 0
   }
 
   // 消息订阅，返回一个id，用于取消订阅
   subscribe(subscriber, watcher = () => {}) {
-    if (this.subsMap.has(subscriber)) {
-      this.subsMap.get(subscriber).push(watcher)
-    } else {
-      this.subsMap.set(subscriber, [watcher])
+    if (!this.subsMap.has(subscriber)) {
+      this.subsMap.set(subscriber, [])
     }
+    this.subsMap.get(subscriber).push(watcher)
     Reflect.set(watcher, '_id', this.id)
+
+    // 离线事件
+    this.offlineStack && this.offlineStack.forEach((fn) => fn()) // TODO 未完成部分
+    this.offlineStack = null
     return this.id++
   }
 
+  // remove
   unsubscribe(subscriber, id) {
-    if (!this.subsMap.has(subscriber)) return
+    if (!this.subsMap.has(subscriber)) return // 如果 subscriber 对应的消息没有被人订阅，则直接返回
     if (id == null) {
       this.subsMap.delete(subscriber)
       return
@@ -37,44 +42,94 @@ class Publisher {
     }
   }
 
+  remove(subscriber, fn) {
+    if (!this.subsMap.has(subscriber)) return // 如果 subscriber 对应的消息没有被人订阅，则直接返回
+    if (!fn) {
+      this.subsMap.delete(subscriber)
+      return
+    }
+    const fns = this.subsMap.get(subscriber)
+    const index = fns.findIndex((item) => item === fn)
+    if (index !== -1) {
+      fns.splice(index, 1)
+    }
+  }
+
+  // trigger
   notify(subscriber, ...rest) {
-    if (!this.subsMap.has(subscriber)) return
-    const watcher = this.subsMap.get(subscriber)
-    watcher.forEach(({ update }) => {
-      typeof update === 'function' && update(...rest)
-    })
+    const fn = () => {
+      if (!this.subsMap.has(subscriber)) return
+      const watcher = this.subsMap.get(subscriber)
+      watcher.forEach(({ subscriber, update }) => {
+        console.log(subscriber, update)
+        typeof update === 'function' && update(...rest)
+      })
+    }
+
+    if (this.offlineStack) {
+      return this.offlineStack.push(fn)
+    }
+    return fn()
   }
 }
 
-const ANTA = new Publisher()
+{
+  const ANTA = new Publisher()
 
-// 订阅运动鞋
-const ids = [
-  ANTA.subscribe('运动鞋', {
+  const fn1 = {
     subscriber: '152xxx',
     update(message) {
       console.log('152xxx' + message)
     },
-  }),
-  ANTA.subscribe('运动鞋', {
+  }
+
+  const fn2 = {
     subscriber: '138yyy',
     update(message) {
       console.log('138yyy' + message)
     },
-  }),
-  ANTA.subscribe('帆布鞋', {
-    subscriber: '139zzz',
-    update(message) {
-      console.log('139zzz' + message)
-    },
-  }),
-]
+  }
 
-// 取消订阅
-ANTA.unsubscribe('运动鞋', 0)
+  console.log(ANTA.offlineStack)
 
-console.log(ANTA.subsMap)
-ANTA.notify('运动鞋', ' 运动鞋到货了 ~') // 打电话通知买家运动鞋消息
-ANTA.notify('帆布鞋', ' 帆布鞋售罄了 T.T') // 打电话通知买家帆布鞋消息
+  ANTA.subscribe('运动鞋', fn1)
+  ANTA.subscribe('运动鞋', fn2)
 
-console.log(ids)
+  // 取消订阅
+  ANTA.remove('运动鞋', fn1)
+
+  ANTA.notify('运动鞋', ' 运动鞋到货了 ~') // 打电话通知买家运动鞋消息
+}
+
+{
+  const ANTA = new Publisher()
+
+  // 订阅运动鞋
+  const ids = [
+    ANTA.subscribe('运动鞋', {
+      subscriber: '152xxx',
+      update(message) {
+        console.log('152xxx' + message)
+      },
+    }),
+    ANTA.subscribe('运动鞋', {
+      subscriber: '138yyy',
+      update(message) {
+        console.log('138yyy' + message)
+      },
+    }),
+    ANTA.subscribe('帆布鞋', {
+      subscriber: '139zzz',
+      update(message) {
+        console.log('139zzz' + message)
+      },
+    }),
+  ]
+
+  console.log(ids)
+  // 取消订阅
+  ANTA.unsubscribe('运动鞋', 0)
+
+  ANTA.notify('运动鞋', ' 运动鞋到货了 ~') // 打电话通知买家运动鞋消息
+  ANTA.notify('帆布鞋', ' 帆布鞋售罄了 T.T') // 打电话通知买家帆布鞋消息
+}
